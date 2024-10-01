63971d1850ce35aa07af1394ff377d5375fc5944
plylrnsdy <plylrnsdy@163.com>
2024-10-01 12:43:31 +0800

    feat: re-index numerical footnote

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,71 @@
 import './style.scss'
-import { Plugin } from '@typora-community-plugin/core'
+import { editor } from 'typora'
+import { Notice, Plugin } from '@typora-community-plugin/core'
 
+
+const RE_REF = /\[\^(\d+)\]/g
+const RE_REF_DEF = /\n\r?\[\^(\d+)\]: .+/g
 
 export default class extends Plugin {
 
   onload() {
-    alert('hello, typora')
+    this.registerCommand({
+      id: 'footnote.reindex',
+      title: 'Footnote: Re-index numberical footnote references',
+      scope: 'editor',
+      callback: () => this.reindex(),
+    })
   }
 
-  onunload() {
-    // dispose resources, like events, processes...
+  reindex() {
+    const notice = new Notice('Footnote re-indexing...', 0)
+
+    const references = editor.nodeMap.foot_list._set.map(node => ({
+      id: node.attributes.ref as string,
+      newId: null,
+      def: '',
+    }))
+      .reduce((o, ref) => (o[ref.id] = ref, o), {} as Record<string, any>)
+
+    const { codeMasker, htmlMasker } = this.app.features.markdownEditor.preProcessor
+
+    codeMasker.reset()
+    htmlMasker.reset()
+
+    let md = editor.getMarkdown()
+    md = codeMasker.mask(md)
+    md = htmlMasker.mask(md)
+
+    let count = 0
+    md = md.replace(RE_REF, (_, id) => {
+      if (references[id]) {
+        if (!references[id].newId) {
+          references[id].newId = String(++count)
+        }
+        return `[^${references[id].newId}]`
+      }
+      return _
+    })
+      .replace(RE_REF_DEF, (_, id) => {
+        if (references[id]) {
+          references[id].def = _
+          return ''
+        }
+        return _
+      })
+      .trimEnd()
+      .concat(Object.values(references).map(ref => ref.def).join(''))
+
+    md = htmlMasker.unmask(md)
+    md = codeMasker.unmask(md)
+
+    const sourceView = editor.sourceView
+    const isSourceMode = sourceView.inSourceMode
+    !isSourceMode && sourceView.show()
+    sourceView.cm.setValue(md)
+    !isSourceMode && sourceView.hide()
+
+    notice.close()
+    new Notice('Footnote re-indexing done.')
   }
 }
--- test/vault/doc.md
+++ test/vault/doc.md
@@ -1,4 +1,23 @@
-# Title
+# Footnote
 
-Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-Adipisci nesciunt dolorem, blanditiis ea facilis, omnis dignissimos, eligendi architecto ducimus veniam necessitatibus illum assumenda id culpa officiis explicabo delectus sequi! Quas.
+- Link 2 [^2] shoud be indexed as 1
+
+- Link 1 [^1] shoud be indexed as 2
+
+- Link Named [^named] should not be changed
+
+`Link 2.1 [^2] shoud not be re-indexed`
+
+```markdown
+Link 2.2 [^2] shoud not be re-indexed
+Link 3 [^3] shoud not be re-indexed
+
+[^3]: https://www.example.com/c
+```
+
+<div><div>Link 2.3 [^2] should not be re-indexed</div></div>
+
+[^1]: https://www.example.com/b
+[^2]: https://www.example.com/a
+[^named]: https://www.example.com/named
+
